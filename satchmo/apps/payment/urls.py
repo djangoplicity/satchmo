a24376be0fcfe7349b47357625c783bf5507410b
Mathias André <mandre@eso.org>
2016-12-15 16:45:30 +0100

    Clean up urls

--- satchmo/apps/payment/urls.py
+++ satchmo/apps/payment/urls.py
@@ -1,23 +1,30 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from django.apps import apps
 from livesettings.functions import config_value
 from satchmo_store.shop.satchmo_settings import get_satchmo_setting
+
+from payment.views.balance import balance_remaining, balance_remaining_order, \
+	charge_remaining, charge_remaining_post
+from payment.views.checkout import success
+from payment.views.contact import authentication_required, contact_info_view
+from payment.views.cron import cron_rebill
+
 import logging
 
 log = logging.getLogger('payment.urls')
 
 ssl = get_satchmo_setting('SSL', default_value=False)
 
-urlpatterns = patterns('payment.views',
-     (r'^$', 'contact.contact_info_view', {'SSL': ssl}, 'satchmo_checkout-step1'),
-     (r'^success/$', 'checkout.success', {'SSL' : ssl}, 'satchmo_checkout-success'),
-     (r'custom/charge/(?P<orderitem_id>\d+)/$', 'balance.charge_remaining', {}, 'satchmo_charge_remaining'),
-     (r'custom/charge/$', 'balance.charge_remaining_post', {}, 'satchmo_charge_remaining_post'),
-     (r'^balance/(?P<order_id>\d+)/$', 'balance.balance_remaining_order', {'SSL' : ssl}, 'satchmo_balance_remaining_order'),
-     (r'^balance/$', 'balance.balance_remaining', {'SSL' : ssl}, 'satchmo_balance_remaining'),
-     (r'^cron/$', 'cron.cron_rebill', {}, 'satchmo_cron_rebill'),
-     (r'^mustlogin/$', 'contact.authentication_required', {'SSL' : ssl}, 'satchmo_checkout_auth_required'),
-)
+urlpatterns = [
+     url(r'^$', contact_info_view, {'SSL': ssl}, 'satchmo_checkout-step1'),
+     url(r'^success/$', success, {'SSL' : ssl}, 'satchmo_checkout-success'),
+     url(r'custom/charge/(?P<orderitem_id>\d+)/$', charge_remaining, {}, 'satchmo_charge_remaining'),
+     url(r'custom/charge/$', charge_remaining_post, {}, 'satchmo_charge_remaining_post'),
+     url(r'^balance/(?P<order_id>\d+)/$', balance_remaining_order, {'SSL' : ssl}, 'satchmo_balance_remaining_order'),
+     url(r'^balance/$', balance_remaining, {'SSL' : ssl}, 'satchmo_balance_remaining'),
+     url(r'^cron/$', cron_rebill, {}, 'satchmo_cron_rebill'),
+     url(r'^mustlogin/$', authentication_required, {'SSL' : ssl}, 'satchmo_checkout_auth_required'),
+]
 
 # now add all enabled module payment settings
 
--- satchmo/apps/product/urls/products.py
+++ satchmo/apps/product/urls/products.py
@@ -1,22 +1,25 @@
-from django.conf.urls import *
+from django.conf.urls import url
 import product
 from satchmo_utils.signals import collect_urls
 
-urlpatterns = patterns('product.views',
-    (r'^(?P<product_slug>[-\w]+)/$', 
-        'get_product', {}, 'satchmo_product'),
-    (r'^(?P<product_slug>[-\w]+)/prices/$', 
-        'get_price', {}, 'satchmo_product_prices'),
-    (r'^(?P<product_slug>[-\w]+)/price_detail/$', 
-        'get_price_detail', {}, 'satchmo_product_price_detail'),
-)
+from product.views import get_product, get_price, get_price_detail
+from product.views.filters import display_recent, display_bestsellers
 
-urlpatterns += patterns('product.views.filters',
-    (r'^view/recent/$', 
-        'display_recent', {}, 'satchmo_product_recently_added'),
-    (r'^view/bestsellers/$', 
-        'display_bestsellers', {}, 'satchmo_product_best_selling'),
-)
+urlpatterns = [
+    url(r'^(?P<product_slug>[-\w]+)/$',
+        get_product, {}, 'satchmo_product'),
+    url(r'^(?P<product_slug>[-\w]+)/prices/$',
+        get_price, {}, 'satchmo_product_prices'),
+    url(r'^(?P<product_slug>[-\w]+)/price_detail/$',
+        get_price_detail, {}, 'satchmo_product_price_detail'),
+]
+
+urlpatterns += [
+    url(r'^view/recent/$',
+        display_recent, {}, 'satchmo_product_recently_added'),
+    url(r'^view/bestsellers/$',
+        display_bestsellers, {}, 'satchmo_product_best_selling'),
+]
 
 # here we are sending a signal to add patterns to the base of the shop.
 collect_urls.send(sender=product, patterns=urlpatterns, section="product")
