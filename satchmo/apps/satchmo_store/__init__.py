13ea5a07ba81293f19ad98a0150ad16809274b26
Mathias André <mandre@eso.org>
2018-01-23 15:25:24 +0100

    Fix version number

--- satchmo/apps/satchmo_store/__init__.py
+++ satchmo/apps/satchmo_store/__init__.py
@@ -1,5 +1,5 @@
-VERSION = (0, 9, '3-eso2', 'pre')
-            
+VERSION = (0, 9, '3+eso2', 'pre')
+
 # Dynamically calculate the version based on VERSION tuple
 if len(VERSION)>2 and VERSION[2] is not None:
     str_version = "%d.%d.%s" % VERSION[:3]
